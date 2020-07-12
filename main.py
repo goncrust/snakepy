716e8a3cff0b315558113485e911f6a2326dd5aa
ricardinho5234 <ricardo.filipe2003@gmail.com>
2020-07-12 02:24:52 +0100

    add snake

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ iconsnake = pygame.image.load("images/snake_icon1.png")
 pygame.display.set_caption("Snake")
 pygame.display.set_icon(iconsnake)
 
+blue=(0,0,255)
+red=(255,0,0)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    pygame.draw.rect(screen,red,[200,150,10,10])
     pygame.display.flip()
