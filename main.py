522b5ebd505d6aca80dc493bb189f16ad3115903
ricardinho5234 <ricardo.filipe2003@gmail.com>
2020-07-12 12:34:08 +0100

    creating food

--- main.py
+++ main.py
@@ -25,3 +25,19 @@ while RUNNING:
             RUNNING = False
     pygame.draw.rect(screen, red, [200, 150, 10, 10])
     pygame.display.flip()
+
+def gameLoop(): # creating a function
+    game_over = False
+    game_close = False
+    x1 = dis_width / 2
+    y1 = dis_heigth / 2
+    
+    x1_change = 0
+    y1_change = 0
+
+    foodx = round(random.randrange(0, dis_width - snake_block) /10.0)
+    foody = round(random.randrange(0, dis_width - snake_block) /10.0)
+
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_heigth or y1 < 0:
+        game_close = True    
+    
