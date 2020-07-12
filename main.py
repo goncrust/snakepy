6404d27530c95f5948ff468d77185a3b560ac35e
teravyte18 <tphp360@gmail.com>
2020-07-12 02:08:20 +0100

    add icon and tab caption, images in folder

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480))
 
 running = True
+iconsnake = pygame.image.load("snake_icon1.png")
+pygame.display.set_caption("Snake")
+pygame.display.set_icon(iconsnake)
 
 while running:
     for event in pygame.event.get():
