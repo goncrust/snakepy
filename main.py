29d8b41ab5031f032630b52231d5caf689631af2
teravyte18 <tphp360@gmail.com>
2020-07-12 12:58:57 +0100

    add screen and snake_block var

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
+screen_width = 640
+screen_height = 480
+snake_block = 10
 
 RUNNING = True
 
@@ -32,18 +36,16 @@ while RUNNING:
     pygame.draw.rect(screen, red, [200, 150, 10, 10])
     pygame.display.flip()
 
-def gameLoop(): # creating a function
-    game_over = False
-    game_close = False
-    x1 = dis_width / 2
-    y1 = dis_heigth / 2
+    x1 = screen_width / 2
+    y1 = screen_height / 2
     
     x1_change = 0
     y1_change = 0
 
-    foodx = round(random.randrange(0, dis_width - snake_block) /10.0)
-    foody = round(random.randrange(0, dis_width - snake_block) /10.0)
+    foodx = round(random.randrange(0, screen_width - snake_block) /10.0)
+    foody = round(random.randrange(0, screen_width - snake_block) /10.0)
 
-    if x1 >= dis_width or x1 < 0 or y1 >= dis_heigth or y1 < 0:
+    if x1 >= screen_width or x1 < 0 or y1 >= screen_height or y1 < 0:
         game_close = True    
+    
     
