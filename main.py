4334be4dc1c1d327b85927f69c7b77a3dd6eca6f
teravyte18 <tphp360@gmail.com>
2020-07-12 11:18:28 +0100

    add whitespaces after commas, UPPER_CASE constant variable, turned soundtrack file to .wav, set music vol to 0.1

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 
-running = True
+RUNNING = True
 
 iconsnake = pygame.image.load("images/snake_icon1.png")
 pygame.display.set_caption("Snake")
 pygame.display.set_icon(iconsnake)
-pygame.mixer.music.load("soundtrack/track 1.mp3")
+pygame.mixer.music.load("soundtrack/Prof Oak.wav")
 pygame.mixer.music.play()
+pygame.mixer.music.set_volume(0.1)
 
-blue=(0,0,255)
-red=(255,0,0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
 
-while running:
+while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-    pygame.draw.rect(screen,red,[200,150,10,10])
+            RUNNING = False
+    pygame.draw.rect(screen, red, [200, 150, 10, 10])
     pygame.display.flip()
