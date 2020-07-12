9bfb512413439bd69f17f23baf5a8cadba4aa992
teravyte18 <tphp360@gmail.com>
2020-07-12 12:47:18 +0100

    draw outline

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ screen = pygame.display.set_mode((640, 480))
 
 RUNNING = True
 
-def move(self):
-    self.rect.top -= self.speed
-
 iconsnake = pygame.image.load("images/snake_icon1.png")
 pygame.display.set_caption("Snake")
 pygame.display.set_icon(iconsnake)
 pygame.mixer.music.load("soundtrack/Prof Oak.wav")
 pygame.mixer.music.play()
-pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.set_volume(0.0)
 
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
+purple = (128, 0, 128)
+
+widht = 11
+pygame.draw.line(screen, purple, (640, 480), (0, 480), widht)
+pygame.draw.line(screen, purple, (0, 480), (0, 0), widht)
+pygame.draw.line(screen, purple, (640, 480), (640, 0), widht)
+pygame.draw.line(screen, purple, (640, 0), (0, 0), widht)
+
+
 
 while RUNNING:
     for event in pygame.event.get():
