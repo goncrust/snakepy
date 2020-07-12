0c51d24a7857e855b0c9d46039653bf790e02aaa
teravyte18 <tphp360@gmail.com>
2020-07-12 11:53:53 +0100

    add green

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ screen = pygame.display.set_mode((640, 480))
 
 RUNNING = True
 
+def move(self):
+    self.rect.top -= self.speed
+
 iconsnake = pygame.image.load("images/snake_icon1.png")
 pygame.display.set_caption("Snake")
 pygame.display.set_icon(iconsnake)
@@ -12,8 +15,9 @@ pygame.mixer.music.load("soundtrack/Prof Oak.wav")
 pygame.mixer.music.play()
 pygame.mixer.music.set_volume(0.1)
 
-blue = (0, 0, 255)
 red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
 
 while RUNNING:
     for event in pygame.event.get():
