15a2def0c4c8871e386e52b3ec1871a3db61c875
goncrust <goncalonrua@gmail.com>
2020-07-12 13:17:17 +0100

    add clear screen, snake initial position, initial automovement and fps

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pygame
+import random
+import time
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 
 RUNNING = True
-
-def move(self):
-    self.rect.top -= self.speed
 
 iconsnake = pygame.image.load("images/snake_icon1.png")
 pygame.display.set_caption("Snake")
@@ -19,9 +18,35 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
+# position
+x = random.randint(0, 427)
+y = random.randint(0, 320)
+pygame.draw.rect(screen, green, [x, y, 10, 10])
+
+# movement
+direction = 0  # 0 - horizontal direita 1 - vertical baixo 2 - horizontal esquerda 3 - vertical cima
+speed = 4
+
+# fps
+fps = 60
+clock = pygame.time.Clock()
+
 while RUNNING:
+
+    clock.tick(60)
+
+    # Quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False
-    pygame.draw.rect(screen, red, [200, 150, 10, 10])
+
+    # GameLoop
+    # Clear Screen
+    screen.fill((0, 0, 0))
+
+    if direction == 0:
+        x += speed
+
+    pygame.draw.rect(screen, green, [x, y, 10, 10])
+
     pygame.display.flip()
