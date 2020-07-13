f802ef479541331ce84b4760b951b986850aa67f
teravyte18 <tphp360@gmail.com>
2020-07-13 23:59:26 +0100

    added images and menu function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+import menu
 
 # Init pygame
 pygame.init()
@@ -17,6 +18,9 @@ GAMEOVER = False
 # Load images
 iconsnake = pygame.image.load("images/snake_icon1.png")
 gameover = pygame.image.load("images/game_over.jpg")
+playbutton = pygame.image.load("images/play.png")
+settingsbutton = pygame.image.load("images/settings.png")
+exitbutton = pygame.image.load("images/exit.png")
 gameoverrect = gameover.get_rect()
 
 # Configure window
@@ -103,6 +107,8 @@ while RUNNING:
 
     # Clear Screen
     screen.fill((0, 0, 0))
+
+    menuScreen()
 
     # Borders
     pygame.draw.line(screen, purple, (screen_width - screenDistance - borderWidht,
---
+++ menu.py
@@ -0,0 +1,9 @@
+import pygame
+import main
+
+def menuScreen():
+    pygame.Surface.blits((playbutton, gameoverrect), (settingsbutton, gameoverrect), (exitbutton, gameoverrect))
+    
+menuScreen()
+    
+    
