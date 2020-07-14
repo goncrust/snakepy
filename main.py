7c2cf411488f5ddb28c452eecf10d3c7150d63d9
goncrust <goncalonrua@gmail.com>
2020-07-14 01:20:30 +0100

    Add snake growth (buggy)

--- main.py
+++ main.py
@@ -40,13 +40,18 @@ blue = (0, 0, 255)
 purple = (128, 0, 128)
 
 # Snake position and movement
+snake = []
 snake_block = 10
-x = random.randint(screenDistance + borderWidht,
+x = random.randint(screenDistance + borderWidht + 10,
                    (screen_width - screenDistance - borderWidht - snake_block)/2)
-y = random.randint(screenDistance + borderWidht,
+y = random.randint(screenDistance + borderWidht + 10,
                    (screen_height - screenDistance - borderWidht - snake_block)/2)
-direction = 0  # 0 - horizontal direita 1 - vertical baixo 2 - horizontal esquerda 3 - vertical cima
-speed = 4
+
+snake.append([x, y])
+snake.append([x - 10, y])
+directions = [0, 0]
+# direction = 0  # 0 - horizontal direita 1 - vertical baixo 2 - horizontal esquerda 3 - vertical cima
+speed = 0.5
 
 # Food
 food_block = 10
@@ -95,13 +100,17 @@ while RUNNING:
             RUNNING = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                direction = 3
+                if directions[0] != 1:
+                    directions[0] = 3
             if event.key == pygame.K_DOWN:
-                direction = 1
+                if directions[0] != 3:
+                    directions[0] = 1
             if event.key == pygame.K_RIGHT:
-                direction = 0
+                if directions[0] != 2:
+                    directions[0] = 0
             if event.key == pygame.K_LEFT:
-                direction = 2
+                if directions[0] != 0:
+                    directions[0] = 2
 
     # --------------- GameLoop -------------------
 
@@ -119,17 +128,21 @@ while RUNNING:
                      (screenDistance, screenDistance), borderWidht)
 
     # Move Snake
-    if direction == 0:
-        x += speed
-    elif direction == 1:
-        y += speed
-    elif direction == 2:
-        x -= speed
-    elif direction == 3:
-        y -= speed
+    index = 0
+    for s in snake:
+        if directions[index] == 0:
+            s[0] += speed
+        elif directions[index] == 1:
+            s[1] += speed
+        elif directions[index] == 2:
+            s[0] -= speed
+        elif directions[index] == 3:
+            s[1] -= speed
+
+        index += 1
 
     # Kill Snake
-    if (x >= screen_width - screenDistance - borderWidht - snake_block) or (x <= screenDistance + borderWidht) or (y >= screen_height - screenDistance - borderWidht - snake_block) or (y <= screenDistance + borderWidht):
+    if (snake[0][0] >= screen_width - screenDistance - borderWidht - snake_block) or (snake[0][0] <= screenDistance + borderWidht) or (snake[0][1] >= screen_height - screenDistance - borderWidht - snake_block) or (snake[0][1] <= screenDistance + borderWidht):
         # RUNNING = False
         GAMEOVER = True
 
@@ -140,16 +153,43 @@ while RUNNING:
         continue
 
     # Snake eat apple
-    if collision(x, y, fx, fy, food_block, food_block, snake_block, snake_block):
+    if collision(snake[0][0], snake[0][1], fx, fy, food_block, food_block, snake_block, snake_block):
+        # Generate new food
         generateFood()
+
+        # Add points
         points += 1
         pygame.display.set_caption("Snake | Points " + str(points))
 
+        # Append snake
+        if directions[len(directions) - 1] == 0:
+            snake.append([int(snake[len(snake) - 1][0]) - 10,
+                          int(snake[len(snake) - 1][1])])
+        elif directions[len(directions) - 1] == 1:
+            snake.append([int(snake[len(snake) - 1][0]),
+                          int(snake[len(snake) - 1][1]) - 10])
+        elif directions[len(directions) - 1] == 2:
+            snake.append([int(snake[len(snake) - 1][0]) + 10,
+                          int(snake[len(snake) - 1][1])])
+        elif directions[len(directions) - 1] == 3:
+            snake.append([int(snake[len(snake) - 1][0]),
+                          int(snake[len(snake) - 1][1]) + 10])
+
+        directions.append(directions[len(directions) - 1])
+
     # Draw Snake
-    pygame.draw.rect(screen, green, [x, y, snake_block, snake_block])
+    for s in snake:
+        pygame.draw.rect(
+            screen, green, [s[0], s[1], snake_block, snake_block])
 
     # Draw food
     pygame.draw.rect(screen, red, [fx, fy, food_block, food_block])
+
+    # Update directions
+    indexDirections = len(directions) - 1
+    while indexDirections != 0:
+        directions[indexDirections] = directions[indexDirections - 1]
+        indexDirections -= 1
 
     # Update Screen
     pygame.display.flip()
